6b5feb62db7e5b178dd34f26b761d6c3442acd92
vishakhamahajan <vmaha25@gmail.com>
2019-03-10 12:11:59 +0530

    Hello
    MVC Demo

---
+++ Hello/Hello/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hello.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.message = "Hello World!!!!";
+            return View();
+        }
+    }
+}
